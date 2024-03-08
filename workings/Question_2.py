0a2c81d1f057aa5057bc6297862e7b6a7119db52
Arvin Karpiah <arvin.karpiah@ucalgary.ca>
2024-03-07 19:53:07 -0700

    Final version

--- workings/Question_2.py
+++ workings/Question_2.py
@@ -1,8 +1,11 @@
-###### Question 2a
+###### Solutions for Question 2.
+
+###### User will be able to choose what variables/plots to display at the bottom of this script 
 
 import numpy as np
 import math
 from math import sqrt
+import matplotlib.pyplot as plt
 
 #Define constants
 
@@ -15,10 +18,9 @@ b = 20
 a = 0
 
 #Generate t
-dt = 2   # Change del_t accordingly
+dt = 1   # Change del_t accordingly
 t = np.linspace(0, 20, num=int(((b-a)/dt)+1))
 N = len(t)-1
-print(t)
 
 #Define function to integrate
 
@@ -30,8 +32,6 @@ V = np.zeros(len(t), dtype=float)
 
 for i in range(len(t)):
     V[i] = v(t[i])
-#print(V)
-#print(V[1:-1])
 
 #Compute integration using trapezium method
 
@@ -45,8 +45,6 @@ total_sum_1 = sum(V[1:-1:2])  # sum of odd elements except the first and last
 total_sum_2 = sum(V[2:-1:2])  # sum of even elements except the first and last
 
 I_simps = ((b - a) / (3 * N)) * (V[0] + 4 * total_sum_1 + 2 * total_sum_2 + V[-1])
-print('for delta_t=',dt,'trapezoid integration=',I_trap)
-print('for delta_t=',dt,'simpsons integration=',I_simps)
 
 #Compute integration using 5-point Gauss Legendre Quadrature
 
@@ -59,7 +57,7 @@ def integrate_gausslegendre(f, lims, npts):
     except (ValueError, TypeError):
         raise ValueError("lims must be convertible to float")
 
-    valid_npts = [1, 2, 3, 4, 5]
+    valid_npts = [5]
     if npts not in valid_npts:
         raise ValueError(f"Invalid value for npts. Must be one of the following values: {valid_npts}")
 
@@ -67,62 +65,6 @@ def integrate_gausslegendre(f, lims, npts):
         # Change limits
         A = (b + a) / 2
         B = (b - a) / 2
-
-        if npts == 1:
-            c_0 = 2.0
-            x_0 = 0.0
-
-            f_0 = f(A + B * x_0)
-                
-            I = B * c_0 * f_0
-       
-
-        elif npts == 2:
-            c_0 = 1
-            c_1 = 1
-            x_0 = -1/sqrt(3)
-            x_1 = 1/sqrt(3)
-    
-
-            f_0=f(A + B * x_0)
-            f_1=f(A + B * x_1)
-
-            I = B*(c_0 * f_0 + c_1 * f_1)
-                  
-
-        elif npts == 3:
-
-            c_0 = 5/9
-            c_1 = 8/9
-            c_2 = 5/9
-            x_0 = -sqrt(3/5)
-            x_1 = 0
-            x_2 = sqrt(3/5)
-
-            f_0=f(A + B * x_0)
-            f_1=f(A + B * x_1)
-            f_2=f(A + B * x_2)
-                
-            I = B*(c_0 * f_0 + c_1 * f_1 + c_2 * f_2)
-
-        if npts == 4:
-
-            c_0 = (18-sqrt(30))/36
-            c_1 = (18+sqrt(30))/36
-            c_2 = (18+sqrt(30))/36
-            c_3 = (18-sqrt(30))/36
-            x_0 = -sqrt(3/7+(2/7)*sqrt(6/5))
-            x_1 = -sqrt(3/7-(2/7)*sqrt(6/5))
-            x_2 = sqrt(3/7-(2/7)*sqrt(6/5))
-            x_3 = sqrt(3/7+(2/7)*sqrt(6/5))
-
-            f_0=f(A + B * x_0)
-            f_1=f(A + B * x_1)
-            f_2=f(A + B * x_2)
-            f_3=f(A + B * x_3)
-    
-            I = B*(c_0 * f_0 + c_1 * f_1 + c_2 * f_2 + c_3 * f_3)
-
 
         if npts == 5:
 
@@ -151,12 +93,19 @@ def integrate_gausslegendre(f, lims, npts):
         raise TypeError("f is not a callable")
 
 I_gauss = integrate_gausslegendre(v,[a,b],5)
-print('using 5 point GLQ',I_gauss)
+
+#Plot for Q2c 
+plt.figure()
+plt.plot(t,V)
+plt.xlabel('t(s)')
+plt.ylabel('v(m/s)')
+plt.title('velocity against time')
 
 
+# Choose which variable/plot to display 
 
-
-
-
-
-
+#print(t)                                                   # To ensure t is sampled at correct dt
+print('for delta_t=',dt,'trapezoid integration=',I_trap)   # View solution using Trapezium Rule
+print('for delta_t=',dt,'simpsons integration=',I_simps)   # View solution using Simpson's 1/3 Rule
+print('using 5 point GLQ=',I_gauss)                         # View solution using Guass-Legendre Quadrature
+#plt.show()                                                 # View plot used for discussion in Q2c
--- workings/Question_3.py
+++ workings/Question_3.py
@@ -1,4 +1,7 @@
-###### Question 3
+###### Solutions for Question 3.
+
+###### By default the code will print roots (in exact form and to 4sf) for fixed point iteration and Newton Raphson 
+###### User will be able to choose other variables/plots to display at the bottom of this script 
 
 import numpy as np
 import math
@@ -16,19 +19,24 @@ Q = 1.29 * 10**5
 
 # Define g1 and g2
 
-
-
-
 def g1(c): 
     return ((W - Q * c)/(k * V)) ** 2
 
 def g2(c): 
     return float(((W - (k * V) * math.sqrt(c)) / Q))
 
+#Checking for convergence
+
+c_true = 2.56
+c_ini = 4
+
+g_prime_ksi = (g2(c_true) - g2(c_ini)) / (c_true - c_ini)
+
+
 # Initialize c and g1(c) and g2(c)
 
 c = np.linspace(1,10,num=100)
-print(c)
+#print("c=",c)
 G1 = np.zeros(len(c), dtype=float)
 G2 = np.zeros(len(c), dtype=float)
 
@@ -44,26 +52,20 @@ fig, axes = plt.subplots(2)  # 2 rows, 1 column
 axes[0].plot(c,c,label="y_1=c")
 axes[0].plot(c,G1,label="y_2=g_1")
 axes[0].set_title('y1=c and y1=g1')
-axes[0].set_xlim(0, 10)  # Set x-axis limits
-axes[0].set_ylim(0, 10)  # Set y-axis limits
+axes[0].set_xlim(1, 10)  # Set x-axis limits
+axes[0].set_ylim(1, 10)  # Set y-axis limits
 axes[0].legend()  # Display legend
-
 
 # Plot data on the second subplot
 axes[1].plot(c,c,label="y_1=c")
 axes[1].plot(c,G2,label="y_2=g_2")
 axes[1].set_title('y1=c and y1=g2')
-axes[1].set_xlim(0, 10)  # Set x-axis limits
-axes[1].set_ylim(0, 10)  # Set y-axis limits
+axes[1].set_xlim(1, 10)  # Set x-axis limits
+axes[1].set_ylim(1, 10)  # Set y-axis limits
 
-# Add labels and title2
 plt.xlabel('x')
 plt.ylabel('y')
-
 plt.legend()
-
-#Show plot
-#plt.show()
 
 ## Part (b)
 
@@ -75,21 +77,18 @@ epsilon_a = np.zeros(1000, dtype=float)
 epsilon_a[0] = 1
 epsilon_s = 0.5*10**-4
 
-
-# # Perform iteration
-i = 0  # Counter initialization
-for i in range(20):
+i = 0  
+for i in range(15):
     c_r[i+1] = g1(c_r[i])
     epsilon_a[i] = abs(c_r[i+1] - c_r[i]) / abs(c_r[i+1])
 
     result_str = "{:.4g}".format(c_r[i])
-    print("Root with FPI=",result_str,"iteration=",i)
 
+    print("Root with FPI=",result_str,"iteration=",i); # View exact root and rounded to 4sf for fixed point iteration method
     print("Root with FPI=",c_r[i],"iteration=",i)
 
-
 #Netwon-Raphson
-
+    
 # Functions
 def f(c):
     return (W - Q*c - k*V*math.sqrt(c))
@@ -97,28 +96,29 @@ def f(c):
 def fprime(c):
     return (-Q - 0.5 * k * V * c**(-0.5))
 
-# Iteration
-for i in range(20):
+for i in range(15):
     c_r[i+1] = c_r[i] - f(c_r[i])/fprime(c_r[i])
     epsilon_a[i+1] = abs(c_r[i+1] - c_r[i]) / abs(c_r[i+1])
 
     result_str = "{:.4g}".format(c_r[i])
-    print("Root with NR=",result_str,"iteration=",i)
 
+    print("Root with NR=",result_str,"iteration=",i) # View exact root and rounded to 4sf for Newton-Raphson method
     print("Root with NR=",c_r[i],"iteration=",i)
+    
 
+#Compute f''(x)'f'(x) near root
 
 def fdoubleprime(c):
     return (0.25 * k * V * c**(-1.5))
 
-#Compute f''(x)'f'(x) near root
-
 A = fdoubleprime(2.55) / fprime(2.55)
-print("A=",A)
 
 
+#print("g_prime_ksi=",g_prime_ksi)                     # View convergence criterion
 
+#plt.show() # View plot used for discussion in Q3a
 
+#print("A=",A) #View additional criteria to check for Newton-Raphson convergence speed
 
 
 
